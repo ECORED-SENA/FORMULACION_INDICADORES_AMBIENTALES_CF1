01a3ecac3c7c85aaab0d964e4c66bd43caef7472
luisEfe80 <luismendez80@hotmail.com>
2021-11-09 18:21:30 -0500

    Adicion de paginas y titulos

---
+++ src/views/Tema4.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal.color-acento-contenido(data-aos='flip-left')
+      .titulo-principal__numero
+        span 4
+      h1 Aspectos e impactos ambientales
+
+    #t_4_1.titulo-segundo.color-secundario
+      h2  4.1 Aspectos ambientales
+
+
+    #t_4_2.titulo-segundo.color-secundario
+      h2  4.2 Impactos ambientales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
