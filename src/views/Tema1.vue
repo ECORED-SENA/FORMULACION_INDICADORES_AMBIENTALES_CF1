68a04bef9ce52b9ac294b35cfc2a967e7036e93f
luisEfe80 <luismendez80@hotmail.com>
2021-11-10 09:59:16 -0500

    Adicion de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -12,7 +12,7 @@
           .row.m-0.align-items-center.justify-content-between
             .col-lg-4.mb-4.mb-lg-0
               figure
-                img(src='@/assets/curso/t1-ic1.svg')
+                img.nW.centered(src='@/assets/curso/t1-ic1.png')
             .col-lg-8
               .bloque-texto-a__texto.p-4.shadow-box
                 p Se habla de fuentes de información o fuentes de documentación para referirse al origen de una información en particular, es decir, el apoyo en el cual se puede acudir para su recuperación. En este sentido, las referencias pueden ser de un tipo muy diferente y de esta forma, proporcionar datos más o menos confiables que serán cruciales y que influyen de manera decisiva en los resultados que se obtengan. Para ello, se cuenta con fuentes de información primaria, secundarias y terciarias que se pueden consultar en el siguiente recurso.
