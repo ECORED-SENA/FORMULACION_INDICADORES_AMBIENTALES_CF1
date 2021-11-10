bcb6cd640a52219513bbb51288d4399822af504b
luisEfe80 <luismendez80@hotmail.com>
2021-11-10 12:03:09 -0500

    Adicion de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    .titulo-principal.color-acento-contenido(data-aos='flip-left')
+    .titulo-principal.color-acento-botones(data-aos='flip-left')
       .titulo-principal__numero
         span 1
       h1 Fuentes de información   
@@ -17,8 +17,35 @@
               .bloque-texto-a__texto.p-4.shadow-box
                 p Se habla de fuentes de información o fuentes de documentación para referirse al origen de una información en particular, es decir, el apoyo en el cual se puede acudir para su recuperación. En este sentido, las referencias pueden ser de un tipo muy diferente y de esta forma, proporcionar datos más o menos confiables que serán cruciales y que influyen de manera decisiva en los resultados que se obtengan. Para ello, se cuenta con fuentes de información primaria, secundarias y terciarias que se pueden consultar en el siguiente recurso.
 
-
-
+    .tarjeta.bg-sliders.p-4.mb-5
+      SlyderA
+        .p-4
+          .row
+            .col-md-6.mb-4.mb-md-0
+              .h4 Fuentes de información primaria
+              p.mb-3  Contienen información original que ha sido publicada por primera vez y que no ha sido filtrada, interpretada o evaluada por nadie más. Son producto de una investigación o de una actividad creativa y componen la colección básica de una biblioteca y pueden encontrarse en soporte impreso o digital.
+              p.mb-3  Entre los tipos más comunes se encuentran: libros, revistas científicas y de entretenimiento, periódicos, diarios, documentos oficiales de instituciones públicas, informes técnicos y de investigación de instituciones públicas o privadas, patentes, normas técnicas.        
+            .col-md-6
+              figure.slideInLeft
+                img(src='@/assets/curso/t1-i1.png', alt='imagen decorativa')
+        .p-4
+          .row
+            .col-md-6.mb-4.mb-md-0
+              .h4 Fuentes de información secundaria
+              p.mb-3  Contienen información primaria, sintetizada y reorganizada. Están diseñadas para facilitar y maximizar el acceso a las fuentes primarias o a sus contenidos. Se utilizan cuando no se tiene acceso a la fuente primaria por una razón específica, cuando los recursos son limitados y cuando la fuente no es confiable.
+              p.mb-3  Entre los tipos más comunes se encuentran: enciclopedias, antologías, directorios, libros o artículos que interpretan otros trabajos o investigaciones. 
+            .col-md-6
+              figure.slideInLeft
+                img(src='@/assets/curso/t1-i2.png', alt='imagen decorativa')
+        .p-4
+          .row
+            .col-md-6.mb-4.mb-md-0
+              .h4 Fuentes de información terciaria
+              p.mb-3  Son guías físicas o virtuales que contienen información sobre las fuentes secundarias y forman parte de la colección de referencia de una biblioteca, además facilitan el control y acceso a toda la gama de repertorios de referencia. 
+              p.mb-3  Entre los tipos más comunes se pueden encontrar: resúmenes, almanaques, bibliografías, cronologías, diccionarios y enciclopedias, directorios, libros de hechos y manuales.
+            .col-md-6
+              figure.slideInLeft
+                img(src='@/assets/curso/t1-i3.png', alt='imagen decorativa')
 
 
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,18 +1,47 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    .titulo-principal.color-acento-contenido(data-aos='flip-left')
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.bg-foot
+    .titulo-principal.color-acento-botones(data-aos='flip-left')
       .titulo-principal__numero
         span 2
       h1 Sistemas de información ambiental   
+    .row.mb-5
+      .col-lg-7.slideInLeft
+        p.mb-4.text-bold.text-green El sistema de información en Colombia denominado SIA, es el encargado de gestionar la información ambiental del país por medio de la integración de actores, procesos, políticas y tecnología que gestionen información acerca del estado, del uso, aprovechamiento, sostenibilidad y vulnerabilidad del medio ambiente, para la educación y participación social con miras a la toma de decisiones que aporten hacia el desarrollo sostenible del país.
+        .cajon.color-acento-botones.p-4.mb-4
+          p.mb-0  El SIA tiene como objetivo identificar los ecosistemas, recursos naturales, o regiones que tienen alta prioridad en acciones de investigación, protección, recuperación o manejo sostenible. Este sistema está liderado por el Ministerio de ambiente y desarrollo sostenible, en coordinación con los institutos de investigación ambiental <b>IDEAM, SINCHI, HUMBOLDT e INVEMAR</b>, las autoridades ambientales regionales, la comunidad académica y todos los usuarios y proveedores de la información ambiental.
+        p.mb-3  Dentro de esta plataforma del sistema de información se pueden encontrar cifras y estadísticas ambientales de diferentes aspectos tales como:
+      .col-lg-5.slideInRight
+        figure
+          img(src='@/assets/curso/t2-i1.jpg')
+
+    .row.mb-5 
+      .col-lg-6(data-aos='flip-left')
+        ImagenInfografica.color-secundario.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/t2-info1.svg', alt='paisaje natural con rios y cascadas')
+
+          .tarjeta.color-acento-botones.p-3(x="20%" y="20%" numero="1")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
+
+      .col-lg-6(data-aos='flip-right')   
+        ImagenInfografica.color-secundario.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/t2-info2.svg', alt='paisaje de ciudad edificios y autopistas')
+
+          .tarjeta.color-acento-botones.p-3(x="20%" y="20%" numero="1")
+            .h5.mb-2 titulo
+            p Lorem ipsum dolor Curabitur vestibulum tortor at blandit congue. Pellentesque vel felis posuere, molestie metus
 
 
-
-
-
-
-
+    .row.mb-5.justify-content-center 
+      .col-lg-8
+        .cajon.color-acento-botones.p-4.mb-4(data-aos='flip-up')
+          p.mb-0 En términos de información ambiental, este sistema es el más utilizado para la recolección, <b>debido a que está en constante actualización y se obtienen datos de todo el territorio nacional.</b>
 
 
 
