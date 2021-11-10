f2eb5587e859bf5eeca58f798e4f477846dc54b8
luisEfe80 <luismendez80@hotmail.com>
2021-11-10 08:32:23 -0500

    Adicion de contenido

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,22 +8,23 @@
         span
           i.fas.fa-info
       h1 Introducción
-    
+
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.custom-cardBlock.slideInUp
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 La formulación de indicadores ambientales requiere de conocimientos básicos de factores ambientales con el fin de apropiar los conceptos de medición y aplicabilidad a continuación podrá identificar elementos básicos del proceso de introducción. Lo invitamos a ver el siguiente video para conocer más sobre la temática a estudiar.
+
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t0-i1.jpg')})`}"
+          )
+            
+
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Leyenda del video
     
-    p.mb-5 La necesidad de mitigar la pandemia por COVID-19 ha obligado a la comunidad científica internacional, a desarrollar una serie de investigaciones y desarrollos tendientes a presentar una vacuna para hacerle frente a esta enfermedad que aumenta día a día su morbi mortalidad en Colombia. Es así, como en diciembre de 2020, la comunidad científica presenta los resultados de varios desarrollos de vacunas, que ofrecen eficacia y seguridad, respaldados por estudios clínicos rigurosos.
-
-    figure.mb-5
-      img(src="@/assets/curso/banner-intro.jpg", alt="Texto que describa la imagen")
-      figcaption Imagen 1. Vacunación
-
-    p.mb-0 La llegada de nuevas vacunas al país supone un reto para el talento humano en salud responsable de la vacunación y desarrollar habilidades para ofrecer una orientación oportuna y segura a las personas objeto de la vacuna.
-      br
-      br
-      | En ese orden de ideas, este componente formativo guía al aprendiz hacia la identificación de los aspectos normativos, técnicos y operativos de la vacunación contra COVID19 en Colombia y de las generalidades de la infección por SARS-2 COVID 19.
 
 </template>
 
