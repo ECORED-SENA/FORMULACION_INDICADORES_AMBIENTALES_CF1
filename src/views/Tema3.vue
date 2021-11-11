e5ab94cc8778b27292fb3431d8cb6ded35edf789
luisEfe80 <luismendez80@hotmail.com>
2021-11-10 19:18:27 -0500

    Adicion de contenido

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -7,10 +7,38 @@
         span 3
       h1 Proyectos, obras y actividades
 
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.slideInUp
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t3-i1.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4.shadow-box
+            p.mb-0 Existen diferentes tipos de proyectos, obras y actividades que requieren de la elaboración de estudios ambientales que están sujetos en su mayoría a un proceso de licenciamiento ambiental; con el propósito de facilitar la elaboración de estos estudios se ha puesto a disposición del público un conjunto de calificaciones de Impactos Ambientales Potenciales (IAP) de tales proyectos, obras o actividades.
 
+    .row.mb-5
+      .col-lg-2
+        figure
+          img(src='@/assets/curso/t3-ic1.svg' alt='imagen decorativa')
+      .col-lg-10
+        p.mb-3  <b>El IAP mide la capacidad que tiene un proyecto, obra o actividad de producir impactos ambientales sobre el medio físico, biótico y socioeconómico de acuerdo con sus características intrínsecas,</b> siendo posible evaluarlo con base en actividades previas o que se encuentran en ejercicio, pues en la medida en que el IAP refleja los impactos característicos de cada tipo de proyecto, obra o actividad, puede ser empleado para estimar de forma preliminar del grado de modificación de las condiciones ambientales de un proyecto en particular.
+        p.mb-3.text-bold  Teniendo en cuenta que se debe cumplir con una normatividad previamente establecida como lo son:
 
+    p.mb-5  En este sentido, es necesario relacionar los recursos naturales que serán objeto de uso, aprovechamiento o afectación, como consecuencia de la realización de las actividades. Por esto, será necesario presentar la información que se requiere especificada a continuación:
 
+    .neg-margin-x.bg-sliders2
+      .row.justify-content-center
+        .col-lg-8
+          ImagenInfografica.color-secundario.mb-5
+            template(v-slot:imagen)
+              figure
+                img(src='@/assets/curso/t3-info1.svg', alt='imagen decorativa')
 
+            .tarjeta.color-acento-botones.p-3(x="0%" y="13%" numero="")
+              .h5.mb-2 titulo
+              p Trámites de autorizaciones
+
+    p.mb-5  En este sentido, es necesario relacionar los recursos naturales que serán objeto de uso, aprovechamiento o afectación, como consecuencia de la realización de las actividades. Por esto, será necesario presentar la información que se requiere especificada a continuación:
 
 
 
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -8,7 +8,7 @@
       h1 Evaluación ambiental
 
     .row.mb-5
-      .col-lg-7
+      .col-lg-7.slideInLeft
         p.mb-4.text-bold.text-green La evaluación ambiental tiene como objetivo la verificación de los parámetros y normativas que actualmente se encuentran vigentes para conocer el estado de los componentes del entorno, lo cual facilita planificar acciones a tomar con el fin de mantener o mejorar las condiciones del medio ambiente.
         .cajon.color-acento-botones.p-4.mb-4
           p.mb-0   La evaluación, al ser uno de los pasos fundamentales en el impacto ambiental, requiere conocer el proceso que se debe realizar, sobre todo al situarlo desde el manejo y utilización de <b>la matriz Leopold</b>.
@@ -18,7 +18,7 @@
           img(src='@/assets/curso/t5-i1.jpg')
 
     .row.mb-5.justify-content-center
-      .col-lg-8
+      .col-lg-8(data-aos='flip-left')
         PasosA.color-acento-botones.mb-5(tipo="l")
           .row.align-items-center
             .col-md-2.mb-4.mb-md-0
@@ -91,24 +91,69 @@
 
     .row.mb-5
       .col-lg-2
-        figure
+        figure(data-aos='flip-left')
           img.nW(src='@/assets/curso/t5-ic11.svg', alt='Texto que describa la imagen')
-      .col-lg-10
+      .col-lg-10(data-aos='slide-left')
         p.mb-3  De acuerdo con lo anterior, es necesario identificar, clasificar y valorar los impactos ambientales generados en cada uno de los procesos de la organización, con el fin de poder determinar las medidas correctivas o preventivas necesarias para la mitigación de estos impactos y así poder generar un proceso sostenible con el medio ambiente en el que se afecte en la menor proporción. 
         p.mb-3  Los proyectos o actividades que deben ser sometidas al <b>Sistema de Evaluación de Impacto Ambiental</b>, son los siguientes:
 
     .row.mb-5
       .col-lg-4
-        figure
+        figure(data-aos='slide-left')
           img.nW(src='@/assets/curso/t5-ic12.jpg', alt='Texto que describa la imagen')
       .col-lg-8  
-
-
-
-
-
-
-
+        ul.lista-ul(data-aos='slide-right')
+          li
+            i.fas.fa-check
+            p.mb-0  Acueductos, presas, drenajes, o aquellas actividades que alteren los cuerpos naturales de agua.
+          li
+            i.fas.fa-check
+            p.mb-0  Líneas de transmisión de energía de alto voltaje.
+          li
+            i.fas.fa-check
+            p.mb-0  Centrales generadoras de energía mayores a 3 MW.  
+          li
+            i.fas.fa-check
+            p.mb-0  Reactores y establecimientos nucleares.  
+          li
+            i.fas.fa-check
+            p.mb-0  Aeropuertos, terminales de buses, automotores, vías férreas, estaciones de servicio y autopistas.              
+          li
+            i.fas.fa-check
+            p.mb-0  Puertos, vías de navegación, astilleros y terminales marítimas.  
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de turismo y desarrollo urbano. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos industriales o inmobiliarios que se ejecuten en zonas declaradas latentes o saturadas. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de desarrollo minero, incluidos los de carbón, petróleo y gas. 
+          li
+            i.fas.fa-check
+            p.mb-0  Oleoductos, gasoductos, ductos mineros u otros análogos. 
+          li
+            i.fas.fa-check
+            p.mb-0  Ejecución de obras, programas o actividades en parques nacionales, reservas nacionales y santuarios de la naturaleza. 
+          li
+            i.fas.fa-check
+            p.mb-0  Agroindustrias, planteles y establos de crianza, lechería y engorda de animales. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de desarrollo o explotación forestal en suelos de bosques nativos. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de explotación intensiva o de cultivos que utilicen el recurso hidrológico. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de saneamiento ambiental.
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos donde se apliquen productos químicos de manera masiva. 
+          li
+            i.fas.fa-check
+            p.mb-0  Proyectos de desarrollo, cultivo o explotación, en las áreas mineras, agrícolas y forestales. 
 
 
 
